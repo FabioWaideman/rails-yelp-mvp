5e5378e48bd570cfd089c2f7c500d6b02c4ecd40
Fabio Waideman <fawaideman@yahoo.com.br>
2023-02-24 17:03:59 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'restaurants#index'
+
+  resources :restaurants, except: %i[edit update destroy] do
+    resources :reviews, shallow: true, only: %i[new create]
+  end
 end
